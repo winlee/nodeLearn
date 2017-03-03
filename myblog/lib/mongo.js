2a86cae8dbed5318b6f5ee78e33c47721061a0bc
winli <winleechina@hotmail.com>
2017-03-03 16:19:43 +0800

    node-myblog continue about post-create

--- myblog/lib/mongo.js
+++ myblog/lib/mongo.js
@@ -1,3 +1,5 @@
+var moment = require('moment');
+var objectIdToTimestamp = require('objectid-to-timestamp');
 var config = require('config-lite');
 var Mongolass = require('mongolass');
 var mongolass = new Mongolass();
@@ -11,4 +13,27 @@ exports.User = mongolass.model('User',{
   bio:{type:'string'}
 });
 
+exports.Post = mongolass.model('Post',{
+  author:{type:Mongolass.Types.ObjectId},
+  title:{type:'string'},
+  content:{type:'string'},
+  pv:{type:'number'}
+});
+
 exports.User.index({name:1}, {unique:true}).exec();
+exports.Post.index({author:1, _id:-1}).exec();
+
+mongolass.plugin('addCreatedAt',{
+  afterFind: function(results){
+    results.forEach(function(item){
+      item.created_at = moment(objectIdToTimestamp(item._id)).format('YYYY-MM-DD HH:mm');
+    });
+    return results;
+  },
+  afterFindOne: function(result){
+    if(result){
+      result.created_at = moment(objectIdToTimestamp(result._id)).format('YYYY-MM-DD HH:mm');
+    }
+    return result;
+  }
+});
--- myblog/routes/signout.js
+++ myblog/routes/signout.js
@@ -5,6 +5,13 @@ var checkLogin = require('../middlewares/check').checkLogin;
 
 // GET /signout 登出
 router.get('/', checkLogin, function(req, res, next) {
+  //清空session用户信息
+  req.session.user = null;
+  req.flash('success','登出成功');
+
+  //登出后跳转首页
+  res.redirect('/posts');
+
   res.send(req.flash());
 });
 
